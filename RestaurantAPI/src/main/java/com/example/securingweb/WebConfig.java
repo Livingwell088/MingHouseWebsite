11d3df94f27b3451dc6e05bca1e07f752e2e495a
Nicholas Li <58043224+Livingwell088@users.noreply.github.com>
2026-03-21 10:15:13 -0400

    Added a CorsConfiguration file

---
+++ RestaurantAPI/src/main/java/com/example/securingweb/WebConfig.java
@@ -0,0 +1,30 @@
+package com.example.securingweb;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class WebConfig {
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "https://minghousema.com",
+                                "https://www.minghousema.com"
+                        ).allowedMethods("*")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+
+}
